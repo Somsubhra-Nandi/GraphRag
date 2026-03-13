1d05de5699966d15ab96b9aafe605e6d061dbb05
Somsubhra-Nandi <nruma325@gmail.com>
2026-03-13 22:36:56 +0530

    final commit of base of semantic search and graph builder pipeline

---
+++ pipeline.py
@@ -0,0 +1,70 @@
+from ingestion.document_loader import DocumentLoader
+from ingestion.cleaner import DocumentCleaner
+from ingestion.chunker import DocumentChunker
+
+from extraction.relation_extractor import RelationExtractor
+from extraction.triple_builder import TripleBuilder
+from graph.graph_builder import GraphBuilder
+
+from vector.embedding_generator import EmbeddingGenerator
+from vector.vector_store import VectorStore
+
+from configs.settings import settings
+from extraction.entity_extractor import EntityExtractor
+
+loader = DocumentLoader()
+cleaner = DocumentCleaner()
+chunker = DocumentChunker()
+
+relation_extractor = RelationExtractor()
+triple_builder = TripleBuilder()
+graph_builder = GraphBuilder()
+entity_extractor = EntityExtractor()
+
+docs = loader.load_documents()
+
+chunks = []
+
+for doc in docs:
+
+    doc["text"] = cleaner.clean(doc["text"])
+
+    chunks.extend(chunker.chunk(doc))
+
+
+# ---------- Embedding pipeline ----------
+
+texts = [c["text"] for c in chunks]
+
+embedder = EmbeddingGenerator(settings.EMBEDDING_MODEL)
+
+embeddings = embedder.generate(texts)
+
+store = VectorStore(len(embeddings[0]))
+
+store.add(embeddings, chunks)
+
+print("Vector store ready")
+
+
+# ---------- Graph extraction ----------
+
+for chunk in chunks:
+
+    text = chunk["text"]
+
+    entities = entity_extractor.extract(text)
+
+    result = relation_extractor.extract(text, entities)
+
+    triples = triple_builder.build(result)
+
+    for t in triples:
+
+        graph_builder.add_triple(
+            t["subject"],
+            t["relation"],
+            t["object"]
+        )
+
+print("Graph construction complete")
