c26fd626833bb7c74a98b75e7e5dc5f377923c8f
Somsubhra-Nandi <nruma325@gmail.com>
2026-03-13 22:26:58 +0530

    graph schema

---
+++ configs/graph_schema.py
@@ -0,0 +1,8 @@
+# These act as a guiding foundation to prevent minor variations (like grouping "Bought" and "Purchased" into "ACQUIRED")
+BASE_ENTITY_TYPES = [
+    "Company", "Person", "Technology", "Product", "Organization", "Location"
+]
+
+BASE_RELATION_TYPES = [
+    "FOUNDED", "ACQUIRED", "DEVELOPED", "INVESTED_IN", "WORKED_AT", "LOCATED_IN"
+]
