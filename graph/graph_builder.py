6c6ffe81b3a78548ce47af7c960d8f99e6b50c4d
Somsubhra-Nandi <nruma325@gmail.com>
2026-03-16 02:01:26 +0530

    phase-5 graph builder

--- graph/graph_builder.py
+++ graph/graph_builder.py
@@ -1,3 +1,4 @@
+import time
 from graph.graph_client import GraphClient
 from graph.entity_resolution import EntityResolver
 
@@ -9,7 +10,17 @@ class GraphBuilder:
         self.client = GraphClient()
         self.resolver = EntityResolver()
 
-    def add_triple(self, subject, relation, obj, subject_type="Entity", object_type="Entity"):
+    def add_triple(
+        self,
+        subject,
+        relation,
+        obj,
+        subject_type="Entity",
+        object_type="Entity",
+        source=None,
+        chunk_id=None,
+        confidence=0.9
+    ):
 
         subject = self.resolver.resolve(subject)
         obj = self.resolver.resolve(obj)
@@ -17,12 +28,21 @@ class GraphBuilder:
         query = f"""
         MERGE (a:{subject_type} {{name:$subject}})
         MERGE (b:{object_type} {{name:$object}})
-        MERGE (a)-[r:`{relation}`]->(b)
+        MERGE (a)-[r:`{relation}` {{
+            source:$source,
+            chunk_id:$chunk_id,
+            confidence:$confidence,
+            timestamp:$timestamp
+        }}]->(b)
         """
 
         params = {
             "subject": subject,
-            "object": obj
+            "object": obj,
+            "source": source,
+            "chunk_id": chunk_id,
+            "confidence": confidence,
+            "timestamp": int(time.time())
         }
 
         self.client.run_query(query, params)
