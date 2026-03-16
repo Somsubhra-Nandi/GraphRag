941110e709c09b79bb8fb977306a7161d961fc28
Somsubhra-Nandi <nruma325@gmail.com>
2026-03-17 01:00:35 +0530

    save memory

--- vector/vector_store.py
+++ vector/vector_store.py
@@ -1,5 +1,6 @@
 import faiss
 import numpy as np
+import pickle
 
 
 class VectorStore:
@@ -12,11 +13,23 @@ class VectorStore:
     def add(self, embeddings, chunks):
 
         self.index.add(np.array(embeddings))
-
         self.texts.extend(chunks)
 
     def search(self, query_embedding, k=5):
 
         distances, indices = self.index.search(query_embedding, k)
-
         return [self.texts[i] for i in indices[0]]
+
+    def save(self, path="vector.index"):
+
+        faiss.write_index(self.index, path)
+
+        with open("vector_chunks.pkl", "wb") as f:
+            pickle.dump(self.texts, f)
+
+    def load(self, path="vector.index"):
+
+        self.index = faiss.read_index(path)
+
+        with open("vector_chunks.pkl", "rb") as f:
+            self.texts = pickle.load(f)
