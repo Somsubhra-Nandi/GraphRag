b8b9725636e2b83f081633022318e6e92de73ed6
Somsubhra-Nandi <nruma325@gmail.com>
2026-03-21 01:36:47 +0530

    updated hybrid retriever with planning

--- retrieval/hybrid_retriever.py
+++ retrieval/hybrid_retriever.py
@@ -1,17 +1,32 @@
 class HybridRetriever:
 
-    def __init__(self, graph_retriever, vector_retriever):
-
+    def __init__(self, graph_retriever, vector_retriever, planner):
         self.graph = graph_retriever
         self.vector = vector_retriever
+        self.planner = planner
 
     def retrieve(self, query):
 
-        graph_context = self.graph.retrieve(query)
+        plan = self.planner.plan(query)
+        print("\n Query Plan:")
+        print(plan)
 
-        vector_context = self.vector.retrieve(query)
+        strategy = plan.get("strategy", "hybrid")
 
-        return {
-            "graph": graph_context,
-            "documents": vector_context
+        results = {
+            "plan": plan,
+            "graph": [],
+            "documents": []
         }
+
+        if strategy == "graph":
+            results["graph"] = self.graph.retrieve(query)
+
+        elif strategy == "vector":
+            results["documents"] = self.vector.retrieve(query)
+
+        else:  # hybrid
+            results["graph"] = self.graph.retrieve(query)
+            results["documents"] = self.vector.retrieve(query)
+
+        return results
