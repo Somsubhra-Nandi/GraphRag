33d4984640d1b7a060e1c2541b792647d0158c19
Somsubhra-Nandi <nruma325@gmail.com>
2026-03-20 18:34:12 +0530

    making namings consistent

--- extraction/triple_builder.py
+++ extraction/triple_builder.py
@@ -1,15 +1,13 @@
 class TripleBuilder:
-
     def build(self, extraction_result):
-
         triples = []
-
-        for relation in extraction_result["relations"]:
-
+        #get the relations list
+        for rel in extraction_result.get("relations", []):
             triples.append({
-                "subject": relation["subject"],
-                "relation": relation["relation"],
-                "object": relation["object"]
+                "subject": rel["subject"],
+                "relation": rel["relation"],
+                "object": rel["object"],
+                #grab properties, or default to an empty dictionary
+                "properties": rel.get("properties", {}) 
             })
-
         return triples
